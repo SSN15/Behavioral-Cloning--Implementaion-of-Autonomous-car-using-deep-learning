5683bde87c1ef5c97b5efa4e66cda86c4a36647d
Shyamnarayan Sankarasubramanian <shyamnarayan93@gmail.com>
2020-05-08 09:44:22 +0200

    Update model.py

--- code/model.py
+++ code/model.py
@@ -64,23 +64,60 @@ def get_model():
     ch, row, col = CH, H, W  # camera format
 
     model = Sequential()
-    model.add(Lambda(lambda x: x/127.5 - 1.,
-        input_shape=(row, col, ch),
-        output_shape=(row, col, ch)))
-    model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode='same', W_regularizer=l2(L2_REG_SCALE)))
-    model.add(ELU())
-    model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode='same', W_regularizer=l2(L2_REG_SCALE)))
-    model.add(ELU())
-    model.add(Convolution2D(64, 5, 5, subsample=(2, 2), border_mode='same', W_regularizer=l2(L2_REG_SCALE)))
-    model.add(Flatten())
-    model.add(Dropout(.2))
-    model.add(ELU())
-    model.add(Dense(512, W_regularizer=l2(0.)))
-    model.add(Dropout(.5))
-    model.add(ELU())
-    model.add(Dense(1, W_regularizer=l2(0.)))
 
-    model.compile(optimizer=Adam(lr=LR), loss='mean_squared_error')
+    # Preprocess incoming data, centered around zero with small standard deviation 
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+
+    # trim image to only see section with road
+    model.add(Cropping2D(cropping=((70,25),(0,0))))           
+
+    #layer 1- Convolution, no of filters- 24, filter size= 5x5, stride= 2x2
+    model.add(Convolution2D(24,5,5,subsample=(2,2)))
+    model.add(Activation('elu'))
+
+    #layer 2- Convolution, no of filters- 36, filter size= 5x5, stride= 2x2
+    model.add(Convolution2D(36,5,5,subsample=(2,2)))
+    model.add(Activation('elu'))
+
+    #layer 3- Convolution, no of filters- 48, filter size= 5x5, stride= 2x2
+    model.add(Convolution2D(48,5,5,subsample=(2,2)))
+    model.add(Activation('elu'))
+
+    #layer 4- Convolution, no of filters- 64, filter size= 3x3, stride= 1x1
+    model.add(Convolution2D(64,3,3))
+    model.add(Activation('elu'))
+
+    #layer 5- Convolution, no of filters- 64, filter size= 3x3, stride= 1x1
+    model.add(Convolution2D(64,3,3))
+    model.add(Activation('elu'))
+
+    #flatten image from 2D to side by side
+    model.add(Flatten())
+
+    #layer 6- fully connected layer 1
+    model.add(Dense(100))
+    model.add(Activation('elu'))
+
+    #Adding a dropout layer to avoid overfitting. Here we are have given the dropout rate as 25% after first fully connected layer
+    model.add(Dropout(0.25))
+
+    #layer 7- fully connected layer 1
+    model.add(Dense(50))
+    model.add(Activation('elu'))
+
+
+    #layer 8- fully connected layer 1
+    model.add(Dense(10))
+    model.add(Activation('elu'))
+
+    #layer 9- fully connected layer 1
+    model.add(Dense(1)) #here the final layer will contain one value as this is a regression problem and not classification
+
+
+    # the output is the steering angle
+    # using mean squared error loss function is the right choice for this regression problem
+    # adam optimizer is used here
+    model.compile(loss='mse',optimizer='adam')
 
     return model
 
